26ed16a6c95e0b9c6a4d28e3e962cf47126756a7
Estefano <estefanoybarra.18@gmail.com>
2026-04-10 12:06:11 -0300

    Punteros 4.f

--- TP1_1.c
+++ TP1_1.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
     printf("\nHola mundo");
+    int a=10;
+    int *puntero=&a;
+    printf("\nEL contenido del puntero es: %d",*puntero);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %p",puntero);
+    printf("\nDireccion de memoria de a es: %p",&a);
+    printf("\nLa direccion de memoria del puntero es: %p",&puntero);
+    printf("\nTamanio de memoria utilizada por la variables es: %d",sizeof(int));
     return 0;
 }
